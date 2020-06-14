f987e2b8918ff81ba9395943a6b8741f7400aa69
Tim Tangonan <trstangonan@gmail.com>
2020-06-14 20:39:16 +0800

    added pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,7 +29,8 @@ exports.createPages = ({ actions, graphql }) => {
     const templates = {
          singlePost: path.resolve('src/templates/single-post.js'),
          tagsPage: path.resolve('src/templates/tags-page.js'),
-         tagPosts: path.resolve('src/templates/tag-posts.js')
+         tagPosts: path.resolve('src/templates/tag-posts.js'),
+         postList: path.resolve('src/templates/post-list.js')
     }
 
     return graphql(`
@@ -95,6 +96,27 @@ exports.createPages = ({ actions, graphql }) => {
                 component: templates.tagPosts,
                 context: {
                     tag
+                }
+            })
+        })
+        
+        const postsPerPage = 2
+        const numberOfPages = Math.ceil(posts.length/postsPerPage)
+
+        Array.from({length: numberOfPages}).forEach((_, index) => {
+            const isFirstPage = index === 0
+            const currentPage = index + 1
+
+            if(isFirstPage) return
+
+            createPage({
+                path: `/page/${currentPage}`,
+                component: templates.postList,
+                context: {
+                     limit: postsPerPage,
+                     skip: index * postsPerPage,
+                     currentPage
+                     // shorthand for currentPage: currentPage
                 }
             })
         })
